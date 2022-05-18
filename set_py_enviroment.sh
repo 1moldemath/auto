853d4ef1c2d0a66750452eb9141add3fdd2aa2ed
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-18 00:25:33 -0300

    using conditional to install pip

--- set_py_enviroment.sh
+++ set_py_enviroment.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# installing stack
+
+PIP=$( pip3 --version )
+sudo apt-get install python3-pip
+
+if [ -z $PIP ]; then
+    echo '---- installing pip -----'
+    sudo apt-get install python3-pip
+    echo '---- pip installed -----'
+else
+    echo 'pip is already intalled.'
+fi
+
 # create directory
 
 DEFAULT_PATH=~/Documents/$1
@@ -9,11 +22,6 @@ mkdir $DEFAULT_PATH
 
 INITIATE_GIT=$( cd $DEFAULT_PATH && git init && git checkout -b main )
 
-
-PYTHON_VERSION=$2
-
-
-echo $PYTHON_VERSION
 
 # creating a virtual environment with Python
 
