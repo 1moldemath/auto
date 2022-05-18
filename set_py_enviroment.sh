37e0b1904b5227e949bb7dfecbf3022232602642
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-18 00:04:08 -0300

    create the directory for the project

--- set_py_enviroment.sh
+++ set_py_enviroment.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Hello World"
+# create directory
+
+DEFAULT_PATH=~/Documents/$1
+mkdir $DEFAULT_PATH
+
+# init git and setting main branch
+
+INITIATE_GIT=$( cd $DEFAULT_PATH && git init )
+
+
+PYTHON_VERSION=$2
+
+
+echo $PYTHON_VERSION
+
+# creating a virtual environment with Python
+
+# install the require version of python
+
+exit
